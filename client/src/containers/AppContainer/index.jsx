07f4972c988f94c4b8d16e9181201a3c84922b6f
kostya007 <kostya.1998.nosarev@mail.ru>
2019-11-22 23:06:38 +0700

    Add basic styles

--- client/src/containers/AppContainer/index.jsx
+++ client/src/containers/AppContainer/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
-
+ 
 import store from './redux/store';
 import Main from './containers/Main';
+
+import 'antd/dist/antd.css';
+import './style.scss';
 
 const AppContainer = () => {
     return (
