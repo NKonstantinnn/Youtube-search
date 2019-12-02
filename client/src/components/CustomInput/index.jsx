5b1cd60976378873810d141be69aedf29b33b367
kostya007 <kostya.1998.nosarev@mail.ru>
2019-12-02 12:11:14 +0700

    Add function of show and hide password in CustomInput component

--- client/src/components/CustomInput/index.jsx
+++ client/src/components/CustomInput/index.jsx
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import {Input, Icon} from 'antd';
 
 import './style.scss';
 
-const CustomInput = ({
-  input,
-  type,
-  size,
-  placeholder,
-  disabled,
-  meta: { touched, error },
-}) => (
-  <div
-    className="custom-input"
-  >
-    <Input className="custom-input__input" {...input} 
-      type={type} size={size} placeholder={placeholder} disabled={disabled} />
-    {
-      (touched && error) && <span className="custom-input__error-msg"><Icon type="warning" />{error}</span>
+const CustomInput = ({input, size, type, placeholder, disabled }) => {
+  const [inputType, setInputType] = useState('text');
+
+  const changeInputType = (type) => {
+    if(type) {
+      setInputType(type);
     }
-  </div>
-);
+    else {
+      setInputType(
+        (prev) => (prev === 'text' ? 'password' : 'text')
+      );
+    }
+  }
+
+  useEffect(() => {
+    changeInputType(type)
+  }, [type]);
+
+  return (
+    <div
+      className="custom-input"
+    >
+      <Input className="custom-input__input" {...input} 
+        type={inputType} size={size} placeholder={placeholder} disabled={disabled} />
+      {
+        (type === "password") && (
+          <span className="custom-input__password-icon" onClick={() => changeInputType()}>
+            <Icon type={inputType === 'password' ? 'eye-invisible' : 'eye'} />
+          </span>
+        )
+      }
+    </div>
+  );
+}
 
 CustomInput.propTypes = {
   type: PropTypes.string.isRequired,
